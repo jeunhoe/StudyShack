e2c58ca808adf198fc0a3c6214389504dc3051d8
Poon Jeun Hoe <e0325449@u.nus.edu>
2019-06-27 21:08:19 +0800

    Deleted HouseListAdapter and Carvdview

--- App folder/StudyShack/app/src/main/java/edu/orbital/studyshack/HouseCardview.java
+++ App folder/StudyShack/app/src/main/java/edu/orbital/studyshack/HouseCardview.java
@@ -15,10 +15,6 @@ public class HouseCardview extends AppCompatActivity {
 
     private ImageView upButton;
     private ImageView addButton;
-    List<House> houses;
-
-    private RecyclerView mRecyclerView;
-    private HouseListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +36,5 @@ public class HouseCardview extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        houses = new LinkedList<>();
-        houses.add(new House("CS2040", "HARDEDST MOD", 1, 20));
-
-        // Get a handle to the RecyclerView.
-        mRecyclerView = findViewById(R.id.recyclerview);
-        // Create an adapter and supply the data to be displayed.
-        mAdapter = new HouseListAdapter(this, houses);
-        // Connect the adapter with the RecyclerView.
-        mRecyclerView.setAdapter(mAdapter);
-        // Give the RecyclerView a default layout manager.
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
