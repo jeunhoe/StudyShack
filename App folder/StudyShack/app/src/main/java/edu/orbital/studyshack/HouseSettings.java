91cc75bb104d28df1a253f16d88fdf55245c7616
Lyndon Lim <lyndonlzj@gmail.com>
2019-06-29 02:24:57 +0800

    Completed housesettings and its changes to database upon delete and edit of names or desc. Improved flow of housesettings back to housecardview

--- App folder/StudyShack/app/src/main/java/edu/orbital/studyshack/HouseSettings.java
+++ App folder/StudyShack/app/src/main/java/edu/orbital/studyshack/HouseSettings.java
@@ -1,7 +1,10 @@
 package edu.orbital.studyshack;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,10 +22,20 @@ public class HouseSettings extends AppCompatActivity {
     String origHouseName;
     String origHouseDesc;
 
+    HouseLevelDbHelper dbH;
+    SQLiteDatabase db;
+    HouseDbHelper dbHspecific;
+    SQLiteDatabase dbspecific;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_settings);
+
+        dbH = new HouseLevelDbHelper(this);
+        db = dbH.getWritableDatabase();
+        dbHspecific = new HouseDbHelper(this);
+        dbspecific = dbHspecific.getWritableDatabase();
 
         mHouseNameEditText = findViewById(R.id.house_settings_name_edit_text);
         mHouseDescEditText = findViewById(R.id.house_settings_description_edit_text);
@@ -38,24 +51,57 @@ public class HouseSettings extends AppCompatActivity {
         mEditButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if((origHouseName.equals(mHouseNameEditText.getText().toString())) &&
+                if ((origHouseName.equals(mHouseNameEditText.getText().toString())) &&
                         (origHouseDesc.equals(mHouseDescEditText.getText().toString()))) {
                     return; //do nothing
                 } else {
                     String newHouseName = mHouseNameEditText.getText().toString();
                     String newHouseDesc = mHouseDescEditText.getText().toString();
 
-                    // Add code to edit both databases
-
+                    //edited HouseLevelDB db
                     // Cardview database
+                    Cursor dbCursor = db.rawQuery("select " + HouseLevelDbHelper.KEY_LEVEL + " from " + HouseLevelDbHelper.TABLE_NAME + " where " +
+                            HouseLevelDbHelper.KEY_NAME + " = ?", new String[]{origHouseName});
+                    dbCursor.moveToFirst();
 
+                    ContentValues changedRow = new ContentValues();
+                    changedRow.put(HouseLevelDbHelper.KEY_NAME, newHouseName);
+                    changedRow.put(HouseLevelDbHelper.KEY_DESC, newHouseDesc);
+                    changedRow.put(HouseLevelDbHelper.KEY_LEVEL, dbCursor.getInt(0));
+                    db.update(HouseLevelDbHelper.TABLE_NAME, changedRow, HouseLevelDbHelper.KEY_NAME + " = ?", new String[]{origHouseName});
+
+                    //edited HouseDB dbspecific
                     // Specific database
+                    Cursor dbSpecificCursor = dbspecific.rawQuery("select * from " + HouseDbHelper.TABLE_NAME + " where " +
+                            HouseDbHelper.KEY_NAME + " = ?", new String[]{origHouseName});
+                    while (dbSpecificCursor.moveToNext()) {
+                        ContentValues editedRow = new ContentValues();
+                        String chosenID = dbSpecificCursor.getInt(0) + "";
+                        editedRow.put(HouseDbHelper.KEY_ID, dbSpecificCursor.getInt(0));
+                        editedRow.put(HouseDbHelper.KEY_DAY, dbSpecificCursor.getInt(1));
+                        editedRow.put(HouseDbHelper.KEY_DATE, dbSpecificCursor.getInt(2));
+                        editedRow.put(HouseDbHelper.KEY_MONTH, dbSpecificCursor.getInt(3));
+                        editedRow.put(HouseDbHelper.KEY_YEAR, dbSpecificCursor.getInt(4));
+                        editedRow.put(HouseDbHelper.KEY_HOUR, dbSpecificCursor.getInt(5));
+                        editedRow.put(HouseDbHelper.KEY_MINS, dbSpecificCursor.getInt(6));
+                        editedRow.put(HouseDbHelper.KEY_NAME, newHouseName);
+                        editedRow.put(HouseDbHelper.KEY_INPUT, dbSpecificCursor.getInt(8));
+                        dbspecific.update(HouseDbHelper.TABLE_NAME, editedRow, HouseDbHelper.KEY_ID + " = ?",
+                                new String[]{chosenID});
+
+                    }
+
 
                     // Dialog pop up
                     AlertDialog.Builder alert = new AlertDialog.Builder(HouseSettings.this);
                     alert.setTitle("Success");
                     alert.setMessage("House successfully changed!");
-                    alert.setNeutralButton("OK", null);
+                    alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            onBackPressed();
+                        }
+                    });
                     alert.setIcon(R.drawable.studyshacklogo);
                     alert.show();
                 }
@@ -73,6 +119,13 @@ public class HouseSettings extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // Delete from database
+                        String dbSelection = HouseLevelDbHelper.KEY_NAME + " LIKE ?";
+                        String[] dbSelectionArg = {origHouseName};
+                        db.delete(HouseLevelDbHelper.TABLE_NAME, dbSelection, dbSelectionArg);
+
+                        String dbSpecificSelection = HouseDbHelper.KEY_NAME + " LIKE ?";
+                        String[] dbSpecificSelectionArg = {origHouseName};
+                        dbspecific.delete(HouseDbHelper.TABLE_NAME, dbSpecificSelection, dbSpecificSelectionArg);
 
                         // Navigate back to cardview
                         Intent intent = new Intent(getApplicationContext(), HouseCardview.class);
@@ -83,6 +136,33 @@ public class HouseSettings extends AppCompatActivity {
                 alert.show();
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        db = dbH.getWritableDatabase();
+        dbspecific = dbHspecific.getWritableDatabase();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        db.close();
+        dbHspecific.close();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        db = dbH.getWritableDatabase();
+        dbspecific = dbHspecific.getWritableDatabase();
 
     }
 }
